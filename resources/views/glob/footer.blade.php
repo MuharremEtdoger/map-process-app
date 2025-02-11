75d334b8b3df67fcf3bd4987179144ea3a930005
Muharrem Etdöğer <muharremetdoger@gmail.com>
2025-02-12 01:32:10 +0300

    Assets Url Update

--- resources/views/glob/footer.blade.php
+++ resources/views/glob/footer.blade.php
@@ -1,10 +1,10 @@
-    <script src="assets/js/jquery-3.7.1.min.js"></script>
+    <script src="/assets/js/jquery-3.7.1.min.js"></script>
     @if(isset($pageAssets['js']))
         @foreach ($pageAssets['js'] as $_js)
             <script src="{{ $_js }}"></script>
         @endforeach
     @endif    
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/map.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/map.js"></script>
 </body>
 </html>
